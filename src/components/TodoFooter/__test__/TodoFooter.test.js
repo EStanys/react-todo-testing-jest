99e68f73eacf019cb6a41ade186feac272548a0f
Edvinas <edvinasstanys@gmail.com>
2021-09-06 13:29:56 +0300

    footer testing done

---
+++ src/components/TodoFooter/__test__/TodoFooter.test.js
@@ -0,0 +1,39 @@
+import { render, screen } from "@testing-library/react";
+import TodoFooter from "../TodoFooter";
+
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom';
+
+function MockTodoFooter({ numberOfIncompleteTasks }) {
+  return (
+    <BrowserRouter>
+      <TodoFooter numberOfIncompleteTasks={numberOfIncompleteTasks} />
+    </BrowserRouter>
+  );
+}
+
+  describe('Footer tests', () => {
+
+  it('Should render correct amount of tasks', () => {
+    render(<MockTodoFooter numberOfIncompleteTasks={3} />);
+
+    const parEl = screen.getByText(/3 tasks left/i)
+    expect(parEl).toBeInTheDocument()
+    expect(parEl).toBeVisible()
+  })
+
+  it('Should render correct single item text', () => {
+    render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+
+    const parEl = screen.getByText(/1 task left/i)
+    expect(parEl).toBeInTheDocument()
+    expect(parEl).toBeVisible()
+  })
+
+  it('Should render correct html el p', () => {
+    render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+
+    const parEl = screen.getByText(/1 task left/i)
+    expect(parEl).toContainHTML('p')
+  })
+})
